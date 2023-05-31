f65fd8c60e074d711cbc6db76e917632381ba400
ll-ss <leesw0529@gmail.com>
2023-05-31 19:17:15 +0900

    ✨ 일정 수정 페이지 생성

--- src/pages/ScheduleEdit.tsx
+++ src/pages/ScheduleEdit.tsx
@@ -1,7 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../components/ScheduleEdit.scss';
+import { FaHeart, FaCommentAlt } from 'react-icons/fa';
 
-function ScheduleEdit() {
-  return <div>ScheduleEdit</div>;
+import { DestinationListComponent } from '../components/ScheduleDetail';
+
+const destinations = [
+  ['제주국제공항', '협재포구', '북촌 돌하르방공원'],
+  ['두문포항', '우도', '지미봉'],
+  ['월정리 해수욕장', '김녕항', '세화해변', '제주국제공항'],
+  ['제주국제공항', '제주국제공항', '제주국제공항']
+];
+
+function ScheduleDetail() {
+  const [checkedDestinations, setCheckedDestinations] = useState(
+    destinations.flat()
+  );
+
+  const onDestinationsChecked = (destinations: string[]) => {
+    setCheckedDestinations(destinations);
+  };
+
+  return (
+    <>
+      <div className='container'>
+        <div className='image-wrapper'>
+          <button type='button' className='image-button'>
+            <img
+              className='image'
+              src='https://images.unsplash.com/photo-1609766418204-94aae0ecfdfc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1032&q=80'
+              alt='representative-image'
+            />
+          </button>
+        </div>
+        <div className='duration'>4일 일정</div>
+        <div className='edit-button-wrapper'>
+          <Link to='/schedule/edit' className='edit-button'>
+            수정하기
+          </Link>
+        </div>
+        <h1 className='title'>혼자 떠나는 우도 여행</h1>
+        <div className='icons-wrapper'>
+          <span id='likes'>
+            <FaHeart id='likes-icon' />7
+          </span>
+          <span id='review-number'>
+            <FaCommentAlt id='review-number-icon' />5
+          </span>
+        </div>
+        <h3 className='writer'>제주123</h3>
+        <h4 className='date'>2023.04.01.</h4>
+        <div className='description-wrapper'>
+          <p>혼자 떠나는 우도 여행 일정입니다.</p>
+        </div>
+        <div className='destinations-wrapper'>
+          <DestinationListComponent
+            destinations={destinations}
+            onChecked={onDestinationsChecked}
+          />
+        </div>
+        <div className='map'>
+          {checkedDestinations.map((dest, index) => (
+            <div key={index}>{dest}</div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default ScheduleEdit;
+export default ScheduleDetail;
