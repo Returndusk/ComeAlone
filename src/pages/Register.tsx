554f3e7f40eb5dde23a511e5a1ccc78de2545418
icallitnewart <icallitnewart@gmail.com>
2023-06-02 02:11:14 +0900

    :lipstick: 회원가입 페이지 UI 구현
    레이아웃 구현 및 디자인 작업 / 추후 작업 예정: 닉네임 중복 확인 버튼 추가, 생년월일 input 방식 변

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import styles from '../components/Register/Register.module.scss';
+import RegisterForm from '../components/Register/RegisterForm';
 
 function Register() {
-  return <div>Register</div>;
+  return (
+    <main className={styles.container}>
+      <div className={styles.wrapper}>
+        <h1>회원가입</h1>
+        <RegisterForm />
+      </div>
+    </main>
+  );
 }
 
 export default Register;
