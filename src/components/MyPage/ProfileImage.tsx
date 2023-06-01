05836c045c29d4a5204c42b800027877559f2da3
icallitnewart <icallitnewart@gmail.com>
2023-06-02 02:22:24 +0900

    :lipstick: 마이페이지 UI 구현
    레이아웃 구현 및 디자인 작업 / 회원 탈퇴 버튼 클릭시 비밀번호 입력 요구

---
+++ src/components/MyPage/ProfileImage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './ProfileImage.module.scss';
+
+function ProfileImage() {
+  return (
+    <div className={styles.profileImage}>
+      <div className={styles.image}></div>
+    </div>
+  );
+}
+
+export default ProfileImage;
