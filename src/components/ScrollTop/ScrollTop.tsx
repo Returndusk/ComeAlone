0e7b927a17c797282b4bac0565b170c5665f7ffd
[SW4/2반] 이지원 <jiwonlee1953@naver.com>
2023-06-02 10:13:13 +0900

    :recycle: 페이지 상단 이동 함수명 변경
    페이지 상단 이동 함수명 변경

--- src/components/ScrollTop/ScrollTop.tsx
+++ src/components/ScrollTop/ScrollTop.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import styles from './ScrollTop.module.scss';
 
 function ScrollTop() {
-  function scrolltoTop() {
+  function scrollToTop() {
     window.scroll({ top: 0, behavior: 'smooth' });
   }
 
   return (
     <div className={styles.scrollButtonBox}>
-      <button className={styles.scrollTopButton} onClick={scrolltoTop}>
+      <button className={styles.scrollTopButton} onClick={scrollToTop}>
         ▲
       </button>
     </div>
