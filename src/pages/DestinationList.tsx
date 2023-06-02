d12bc97e1d984950a15c540519c6da68d17db73b
Jeongeun Lee <leeju1009@gmail.com>
2023-06-02 16:27:03 +0900

    :bulb: 주석 수정
    page 파일의 주석 수정

--- src/pages/DestinationList.tsx
+++ src/pages/DestinationList.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
-// import AddToScheduleModal from '../components/Modals/AddToScheduleModal';
+import AddToScheduleModal from '../components/DestinationList/Modal/AddToScheduleModal';
 import Layout from '../components/DestinationList/Layout';
 import Search from '../components/DestinationList/Search';
 
 function DestinationList() {
   return (
-    <>
-      <Layout>
-        <Search />
-      </Layout>
-      {/* <AddToScheduleModal /> */}
-    </>
+    <Layout>
+      <Search />
+      <AddToScheduleModal />
+    </Layout>
   );
 }
 
