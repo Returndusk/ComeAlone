d1f8fd6f375677e17528049c5c0a18c1e98ddd71
Jeongeun Lee <leeju1009@gmail.com>
2023-06-03 21:40:59 +0900

    :lipstick: 좋아요 여부를 표시하는 아이콘 추가
    좋아요를 토글 형식으로 추가하고 취소하는 아이콘 추가

--- src/components/DestinationList/DestinationDetails.tsx
+++ src/components/DestinationList/DestinationDetails.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { DestinationsType } from './Types';
 import styles from './DestinationDetails.module.scss';
+import { RiThumbUpFill } from 'react-icons/ri';
+import { RiThumbUpLine } from 'react-icons/ri';
 
 type DestinationDetailsPropsType = {
   clickedDestination: DestinationsType | null;
@@ -14,6 +16,7 @@ function DestinationDetails({
   setClickedDestination
 }: DestinationDetailsPropsType) {
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [likes, setLikes] = useState<boolean>(false);
   const destination = clickedDestination;
 
   const toggleDetailPage = () => {
@@ -27,25 +30,41 @@ function DestinationDetails({
     }
   }, [clickedDestination]);
 
+  const handleLikesClick = () => {
+    /*좋아요 요청(백엔드 요청)*/
+    setLikes(() => !likes);
+  };
+
   return (
     <>
       {isOpen && destination !== null && (
         <div className={styles.destinationDetailsContainer}>
+          <div className={styles.detailsButtonContainer}>
+            <button
+              className={styles.detailsCloseButton}
+              onClick={toggleDetailPage}
+            >
+              X
+            </button>
+          </div>
           <section className={styles.destinationDetails}>
             <h3>{destination.title}</h3>
             <p>전화번호:{destination?.tel}</p>
             <div>{destination?.overview}</div>
-            <div>
-              <button>내 일정에 추가</button>
-            </div>
-          </section>
 
-          <button
-            className={styles.detailsButtonContainer}
-            onClick={toggleDetailPage}
-          >
-            상세 페이지 닫기
-          </button>
+            <button
+              className={styles.detailsLikesButton}
+              onClick={handleLikesClick}
+            >
+              {likes ? (
+                <RiThumbUpFill className={styles.detailsLikesUpButton} />
+              ) : (
+                <RiThumbUpLine className={styles.detailsLikesCancelButton} />
+              )}
+            </button>
+            <div>{/*<button>내 일정에 추가</button>*/}</div>
+          </section>
+          <section className={styles.detailsReviewsContainer}></section>
         </div>
       )}
     </>
