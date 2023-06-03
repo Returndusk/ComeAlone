d60f68bdfa66459aae5faa5796861b297a68696f
icallitnewart <icallitnewart@gmail.com>
2023-06-04 05:40:58 +0900

    :sparkles: 회원정보수정 페이지 비밀번호 확인 유효성 검사 적용

--- src/components/UserEdit/UserConfirmForm.tsx
+++ src/components/UserEdit/UserConfirmForm.tsx
@@ -7,17 +7,33 @@ interface UserConfirmFormProps {
 
 function UserConfirmForm({ confirmUser }: UserConfirmFormProps) {
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setPassword(e.target.value);
   };
 
-  const attemptSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const validateForm = (password: string) => {
+    let errMsg = '';
+    if (!password) errMsg = '비밀번호를 입력해주세요.';
+    return errMsg;
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    confirmUser();
+    const validationError = validateForm(password);
+    setError(validationError);
+
+    if (!validationError) {
+      alert('유효성 검사 통과!');
+      //비밀번호 확인 api 호출
+
+      //비밀번호 확인 성공시
+      confirmUser();
+    }
   };
 
   return (
-    <form className={styles.form} onSubmit={attemptSubmit}>
+    <form className={styles.form} onSubmit={handleSubmit}>
       <div className={styles.input}>
         <label htmlFor='password'>비밀번호 확인</label>
         <input
@@ -27,6 +43,7 @@ function UserConfirmForm({ confirmUser }: UserConfirmFormProps) {
           value={password}
           onChange={handleChange}
         />
+        {error && <p className={styles.errMsg}>{error}</p>}
       </div>
       <button type='submit'>확인</button>
     </form>
