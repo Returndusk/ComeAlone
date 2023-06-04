d4f9122bb3796dcd8a89a9cbd4bb25f0880a0d37
[SW4/2반] 이지원 <jiwonlee1953@naver.com>
2023-06-04 14:17:53 +0900

    :recycle: 라우팅 path 상수 파일로 관리
    라우팅 path 상수 파일로 분리하여 관리

---
+++ src/constants/Router.tsx
@@ -0,0 +1,12 @@
+export default {
+  MAIN: '/',
+  LOGIN: '/login',
+  REGISTER: '/register',
+  MYPAGE: '/mypage',
+  MYPAGE_EDIT: '/mypage/edit',
+  DESTINATION_LIST: '/destination/list',
+  SCHEDULE_LIST: '/schedule/list',
+  MYSCHEDULE_LIST: '/myschedule/list',
+  SCHEDULE_EDIT: '/schedule/edit',
+  SCHEDULE_DETAIL: '/schedule/detail'
+};
