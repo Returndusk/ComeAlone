f65b0a6f56179f7826a2db13242edb10e2579941
[SW4/2반] 이지원 <jiwonlee1953@naver.com>
2023-06-05 04:50:16 +0900

    :sparkles: My Schedule List 페이지 기본 레이아웃 구
    My Schedule List 페이지 기본 레이아웃 구성

---
+++ src/components/ScheduleList/MyScheduleList.tsx
@@ -0,0 +1,86 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import styles from './ScheduleList.module.scss';
+import ScheduleCard from '../ScheduleCard/ScheduleCard';
+import axios from 'axios';
+import ScheduleListDummy from './ScheduleListDummy';
+import { ScheduleType, ScheduleListType } from './ScheduleList';
+import ROUTER from '../../constants/Router';
+
+function MyScheduleLists() {
+  const [scheduleList, setScheduleList] = useState<ScheduleListType>([]);
+  const [scheduleSort, setScheduleSort] = useState<string>('upcoming');
+
+  const fetchData = useCallback(async () => {
+    const API_URL = 'http://localhost:9999/data';
+    try {
+      const reponse = await axios.get(API_URL);
+      setScheduleList(reponse.data);
+    } catch (error: unknown) {
+      if (axios.isAxiosError(error)) {
+        setScheduleList(ScheduleListDummy);
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  function handleSort(e: React.MouseEvent<HTMLButtonElement>) {
+    const sortOption = (e.target as HTMLButtonElement).value;
+    setScheduleSort(sortOption);
+  }
+
+  return (
+    <div className={styles.scheduleContainer}>
+      <div className={styles.scheduleListTitle}>나의 여행 일정</div>
+      <div className={styles.scheduleFilter}>
+        <button
+          className={`${styles.sortButton} ${
+            scheduleSort === 'upcoming' ? styles.selected : ''
+          }`}
+          onClick={(e) => {
+            handleSort(e);
+          }}
+          value='upcoming'
+        >
+          다가오는 일정
+        </button>
+        <button
+          className={`${styles.sortButton} ${
+            scheduleSort === 'past' ? styles.selected : ''
+          }`}
+          onClick={(e) => {
+            handleSort(e);
+          }}
+          value='past'
+        >
+          지난 일정
+        </button>
+        <button
+          className={`${styles.sortButton} ${
+            scheduleSort === 'like' ? styles.selected : ''
+          }`}
+          onClick={(e) => {
+            handleSort(e);
+          }}
+          value='like'
+        >
+          좋아요 한 일정
+        </button>
+      </div>
+      <div className={styles.scheduleCardContainer}>
+        {scheduleList.map((schedule: ScheduleType, index: number) => (
+          <ScheduleCard
+            schedule={schedule}
+            key={index}
+            link={ROUTER.SCHEDULE_DETAIL}
+          />
+        ))}
+        <div className={styles.scheduleAdd}>일정 추가하기</div>
+      </div>
+    </div>
+  );
+}
+
+export default MyScheduleLists;
