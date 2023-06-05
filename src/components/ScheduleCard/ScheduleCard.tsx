c14f4ba9ad1d887214bcbf1f2a5216fb19b30a76
[SW4/2반] 이지원 <jiwonlee1953@naver.com>
2023-06-05 15:43:16 +0900

    :recycle: Schedule 관련 types 분리
    ScheduleTypes 파일로 분리

--- src/components/ScheduleCard/ScheduleCard.tsx
+++ src/components/ScheduleCard/ScheduleCard.tsx
@@ -1,41 +1,39 @@
 import React from 'react';
 import styles from './ScheduleCard.module.scss';
-import { ScheduleType } from '../ScheduleList/ScheduleList';
 import { Link } from 'react-router-dom';
+import { ScheduleCardProps } from '../../types/ScheduleTypes';
 
-type PropsType = {
-  schedule: ScheduleType;
-  link: string;
-};
-
-function ScheduleCard({ schedule, link }: PropsType) {
-  const endDate = new Date(schedule.end_date);
-  const startDate = new Date(schedule.start_date);
-  const createdAt = schedule.createdAt;
-  const diffTime = endDate.getTime() - startDate.getTime();
-  const diffDate = Math.floor(diffTime / (24 * 60 * 60 * 1000));
-
+function ScheduleCard({ schedule, link }: ScheduleCardProps) {
   return (
     <Link to={link} className={styles.scheduleCard}>
       <div className={styles.scheduleCardContent}>
         <div className={styles.scheduleContent}>
           <div className={styles.scheduleText}>
             <div className={styles.scheduleTitle}>{schedule.title}</div>
-            <div className={styles.scheduleDescription}>
-              {schedule.description}
-            </div>
+            <div className={styles.scheduleDescription}>{schedule.summary}</div>
             <div className={styles.scheduleDate}>
-              {diffDate}박 {diffDate + 1}일
+              {schedule.duration - 1}박 {schedule.duration}일
             </div>
             <div className={styles.scheduleAuthor}>
-              작성자 : {schedule.createdBy}
+              작성자 : {schedule.nickname}
             </div>
-            <div className={styles.scheduleCreated}>등록 : {createdAt}</div>
+            <div className={styles.scheduleCreated}>
+              등록 : {schedule.createdAt}
+            </div>
           </div>
-          <div className={styles.scheduleLike}>❤ {schedule.likes}</div>
+          <div className={styles.scheduleLike}>❤ 좋아요 수</div>
         </div>
         <div className={styles.scheduleDestination}>
-          {schedule.destinations.join('=>')}
+          <div>{schedule.destinations[0]}</div>
+          <div>{'=>'}</div>
+          <div className={styles.destinationCount}>
+            경유{' '}
+            {schedule.destinations.length - 2 < 0
+              ? 0
+              : schedule.destinations.length - 2}
+          </div>
+          <div>{'=>'}</div>
+          <div>{schedule.destinations[schedule.destinations.length - 1]}</div>
         </div>
         <img src={schedule.image} className={styles.image} />
       </div>
