1fbe253c5f4f273e71820e8f3b5871ae77732d30
[SW4/2반] 이지원 <jiwonlee1953@naver.com>
2023-06-05 15:45:34 +0900

    :sparkles: Schedule Card 별 기능 분리
    My Schedule Card 기능 분리

---
+++ src/components/ScheduleCard/MyScheduleCard.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import styles from './ScheduleCard.module.scss';
+import { Link } from 'react-router-dom';
+import { MyScheduleCardProps } from '../../types/ScheduleTypes';
+
+function ScheduleCard({ schedule, link }: MyScheduleCardProps) {
+  return (
+    <Link to={link} className={styles.scheduleCard}>
+      <div className={styles.scheduleCardContent}>
+        <div className={styles.scheduleContent}>
+          <div className={styles.scheduleText}>
+            <div className={styles.scheduleTitle}>{schedule.title}</div>
+            <div className={styles.scheduleDescription}>{schedule.summary}</div>
+            <div className={styles.scheduleDate}>
+              {schedule.duration - 1}박 {schedule.duration}일
+            </div>
+            <div>{schedule.start_date}</div>
+            <div>{schedule.end_date}</div>
+            <div className={styles.scheduleCreated}>
+              등록 : {schedule.createdAt}
+            </div>
+          </div>
+          <div className={styles.scheduleLike}>❤ 좋아요 수</div>
+        </div>
+        <div className={styles.scheduleDestination}>
+          <div>{schedule.destinations[0]}</div>
+          <div>{'=>'}</div>
+          <div className={styles.destinationCount}>
+            경유{' '}
+            {schedule.destinations.length - 2 < 0
+              ? 0
+              : schedule.destinations.length - 2}
+          </div>
+          <div>{'=>'}</div>
+          <div>{schedule.destinations[schedule.destinations.length - 1]}</div>
+        </div>
+        <img src={schedule.image} className={styles.image} />
+      </div>
+    </Link>
+  );
+}
+
+export default ScheduleCard;
