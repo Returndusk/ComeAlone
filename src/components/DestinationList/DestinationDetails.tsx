d0cade1d3735d56ef98fdc53022568e70e168837
Jeongeun Lee <leeju1009@gmail.com>
2023-06-05 13:12:57 +0900

    :recycle: 목적지 상세정보 창에 파라미터를 이용한 라우팅 적용
    목적지 상세정보창에 파라미터를 이용한 라우팅 적용(검색 기능과 같이 사용하면 url이 꼬이는 버그 있음)

--- src/components/DestinationList/DestinationDetails.tsx
+++ src/components/DestinationList/DestinationDetails.tsx
@@ -1,29 +1,77 @@
-import React, { useState } from 'react';
-import { DestinationsType } from './Types';
+import React, { useMemo, useState } from 'react';
 import styles from './DestinationDetails.module.scss';
 import { RiThumbUpFill } from 'react-icons/ri';
 import { RiThumbUpLine } from 'react-icons/ri';
 import Review from './Review';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
 
-type DestinationDetailsPropsType = {
-  clickedDestination: DestinationsType | null;
-  setClickedDestination: React.Dispatch<
-    React.SetStateAction<DestinationsType | null>
-  >;
-  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
-};
+const DEFAULT_DESTINATIONS = [
+  {
+    title: '제주도 시청',
+    mapy: 33.48907969999994,
+    mapx: 126.49932809999973,
+    tel: '064-772-3366',
+    overview: '개요 설명입니다.',
+    contenttypeid: '32',
+    contentid: '0'
+  },
+  {
+    title: '한라산',
+    mapy: 33.37915262371278,
+    mapx: 126.54626368383182,
+    tel: '064-772-3366',
+    overview: '개요 설명입니다.',
+    contenttypeid: '12',
+    contentid: '1'
+  },
+  {
+    title: '서귀포 해양 도립공원',
+    mapy: 33.241570451808215,
+    mapx: 126.55770550692283,
+    tel: '064-772-3366',
+    overview: '개요 설명입니다.',
+    contenttypeid: '25',
+    contentid: '2'
+  },
+  {
+    title: '금오름',
+    mapy: 33.35452764241429,
+    mapx: 126.30590904987518,
+    tel: '064-772-3366',
+    overview: '개요 설명입니다.',
+    contenttypeid: '12',
+    contentid: '3'
+  }
+];
 
-function DestinationDetails({
-  clickedDestination,
-  setClickedDestination,
-  setIsOpen
-}: DestinationDetailsPropsType) {
+function DestinationDetails() {
   const [likes, setLikes] = useState<boolean>(false);
-  const destination = clickedDestination;
+  const navigate = useNavigate();
+  const { contentid } = useParams();
+  const { search } = useLocation();
+  // const search = location.search ?? '';
 
-  const toggleDetailPage = () => {
-    setIsOpen(() => false);
-    setClickedDestination(() => null);
+  const destination = useMemo(() => {
+    return DEFAULT_DESTINATIONS.find((des) => des.contentid === contentid);
+  }, [contentid]);
+
+  const searchQuery = useMemo(() => {
+    return search;
+  }, [search]);
+
+  // const removePathParam = (search: string) => {
+  //   const searchParams = new URLSearchParams(search);
+  //   // console.log(searchParams, 'searchParams');
+  //   // searchParams.delete(`${contentid}`);
+  //   // return `?${searchParams.toString()}`;
+  //   return searchParams.toString();
+  // };
+
+  const closeDetailPage = () => {
+    const newUrl = searchQuery
+      ? `/destination/list?search=${searchQuery}`
+      : `/destination/list`;
+    navigate(newUrl, { replace: true });
   };
 
   const handleLikesClick = () => {
@@ -38,13 +86,13 @@ function DestinationDetails({
           <div className={styles.detailsButtonContainer}>
             <button
               className={styles.detailsCloseButton}
-              onClick={toggleDetailPage}
+              onClick={closeDetailPage}
             >
               X
             </button>
           </div>
           <section className={styles.destinationDetails}>
-            <h2>{destination.title}</h2>
+            <h2>{destination?.title}</h2>
             <p>전화번호:{destination?.tel}</p>
             <div>{destination?.overview}</div>
 
@@ -61,7 +109,8 @@ function DestinationDetails({
             <div>{/*<button>내 일정에 추가</button>*/}</div>
           </section>
           <section className={styles.detailsReviewsContainer}>
-            <Review clickedDestination={clickedDestination} />
+            {/* <Review clickedDestination={clickedDestination} /> */}
+            <Review clickedDestination={destination ?? null} />
           </section>
         </div>
       )}
