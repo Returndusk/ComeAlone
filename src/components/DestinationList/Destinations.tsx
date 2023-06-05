22551f94ee79ef272625d8b740b3846d69c816d6
Jeongeun Lee <leeju1009@gmail.com>
2023-06-06 00:44:16 +0900

    :fire: 콘솔 코드 삭제
    콘솔 로그(디버깅) 코드 삭제

--- src/components/DestinationList/Destinations.tsx
+++ src/components/DestinationList/Destinations.tsx
@@ -24,8 +24,6 @@ function Destinations({ filteredDestinations }: DestinationsPropsType) {
   const { search } = useLocation();
   const navigate = useNavigate();
 
-  console.log(filteredDestinations, '데스티네이션 filteredDestinations');
-
   useEffect(() => {
     setDetailsDomRoot(() => document.getElementById('main'));
   }, []);
