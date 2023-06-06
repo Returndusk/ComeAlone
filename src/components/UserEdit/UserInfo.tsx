eadbadcdec6d3d2a2e330642cb46aa441bb99f2b
icallitnewart <icallitnewart@gmail.com>
2023-06-06 14:35:16 +0900

    :lipstick: 회원정보수정 페이지 input창 MUI로 변경

--- src/components/UserEdit/UserInfo.tsx
+++ src/components/UserEdit/UserInfo.tsx
@@ -1,29 +1,35 @@
 import React from 'react';
 import styles from './UserEditForm.module.scss';
 import { UserInfoProps } from '../../types/UserTypes';
+import TextField from '@mui/material/TextField';
+import { FormControlLabel, FormLabel, RadioGroup, Radio } from '@mui/material';
 
 function UserInfo({ values, errors, handleChange }: UserInfoProps) {
   return (
     <ul className={styles.inputs}>
       <li>
-        <label htmlFor='email'>이메일</label>
-        <input
-          type='text'
-          id='email'
+        <TextField
+          id='outlined-basic'
+          label='이메일'
+          variant='outlined'
           name='email'
           value={values.email}
+          onChange={handleChange}
+          size='small'
+          style={{ width: '100%' }}
           disabled
         />
       </li>
       <li>
-        <label htmlFor='nickname'>닉네임</label>
         <div className={styles.nickname}>
-          <input
-            type='text'
+          <TextField
+            id='outlined-basic'
+            label='닉네임'
+            variant='outlined'
             name='nickname'
-            id='nickname'
             value={values.nickname}
             onChange={handleChange}
+            size='small'
           />
           <button type='button'>중복확인</button>
         </div>
@@ -35,13 +41,16 @@ function UserInfo({ values, errors, handleChange }: UserInfoProps) {
         {errors.nickname && <p className={styles.errMsg}>{errors.nickname}</p>}
       </li>
       <li>
-        <label htmlFor='password'>비밀번호 변경</label>
-        <input
+        <TextField
+          id='outlined-basic'
+          label='비밀번호 변경'
           type='password'
-          id='newPassword'
+          variant='outlined'
           name='newPassword'
           value={values.newPassword}
           onChange={handleChange}
+          size='small'
+          style={{ width: '100%' }}
         />
         {!errors.newPassword && (
           <p className={styles.msg}>
@@ -53,64 +62,70 @@ function UserInfo({ values, errors, handleChange }: UserInfoProps) {
         )}
       </li>
       <li>
-        <label htmlFor='passwordConfirm'>비밀번호 확인</label>
-        <input
+        <TextField
+          id='outlined-basic'
+          label='비밀번호 확인'
           type='password'
+          variant='outlined'
           name='passwordConfirm'
-          id='passwordConfirm'
           value={values.passwordConfirm}
           onChange={handleChange}
+          size='small'
+          style={{ width: '100%' }}
         />
         {errors.passwordConfirm && (
           <p className={styles.errMsg}>{errors.passwordConfirm}</p>
         )}
       </li>
+      <li className={styles.radioGroup}>
+        <FormLabel id='demo-radio-buttons-group-label'>성별</FormLabel>
+        <RadioGroup
+          aria-labelledby='demo-radio-buttons-group-label'
+          defaultValue='male'
+          name='gender'
+          onChange={handleChange}
+          row
+        >
+          <FormControlLabel
+            value='male'
+            control={<Radio size='small' />}
+            label='남성'
+            checked={values.gender === 'male'}
+          />
+          <FormControlLabel
+            value='female'
+            control={<Radio size='small' />}
+            label='여성'
+            checked={values.gender === 'female'}
+          />
+        </RadioGroup>
+        {errors.gender && <p className={styles.errMsg}>{errors.gender}</p>}
+      </li>
       <li>
-        <label htmlFor='birthDate'>생년월일</label>
-        <input
-          type='text'
+        <TextField
+          id='outlined-basic'
+          label='생년월일'
+          variant='outlined'
           name='birthDate'
-          id='birthDate'
           value={values.birthDate}
           onChange={handleChange}
+          size='small'
+          style={{ width: '100%' }}
         />
         {errors.birthDate && (
           <p className={styles.errMsg}>{errors.birthDate}</p>
         )}
       </li>
       <li>
-        <label htmlFor='gender'>성별</label>
-        <div className={styles.radioGroup}>
-          <label>
-            <input
-              type='radio'
-              name='gender'
-              value='male'
-              checked={values.gender === 'male'}
-              onChange={handleChange}
-            />
-            남자
-          </label>
-          <label>
-            <input
-              type='radio'
-              name='gender'
-              value='female'
-              checked={values.gender === 'female'}
-              onChange={handleChange}
-            />
-            여자
-          </label>
-        </div>
-      </li>
-      <li>
-        <label htmlFor='phoneNumber'>연락처</label>
-        <input
-          type='text'
+        <TextField
+          id='outlined-basic'
+          label='연락처'
+          variant='outlined'
           name='phoneNumber'
-          id='phoneNumber'
           value={values.phoneNumber}
           onChange={handleChange}
+          size='small'
+          style={{ width: '100%' }}
         />
         {!errors.phoneNumber && <p className={styles.msg}>하이픈(-) 포함</p>}
         {errors.phoneNumber && (
