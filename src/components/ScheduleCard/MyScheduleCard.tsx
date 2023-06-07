aa0d254371e6cb008aa7223ad9fabcc21a36d2ad
[SW4/2반] 이지원 <jiwonlee1953@naver.com>
2023-06-07 21:16:39 +0900

    :bug: 여행 일정 타입 수정에 따른 변경
    여행 일정 타입 수정에 따른 변경

--- src/components/ScheduleCard/MyScheduleCard.tsx
+++ src/components/ScheduleCard/MyScheduleCard.tsx
@@ -1,31 +1,21 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import styles from './ScheduleCard.module.scss';
 import { Link } from 'react-router-dom';
 import { MyScheduleCardProps } from '../../types/ScheduleTypes';
 import { FaTrashAlt } from 'react-icons/fa';
 
 function ScheduleCard({ schedule, link }: MyScheduleCardProps) {
-  const [firstDestination, setFirstDestination] = useState<string>('');
-  const [lastDestination, setLastDestination] = useState<string>('');
-  const [destinationCount, setDestinationCount] = useState<number>(0);
-
-  useEffect(() => {
-    const destinations = schedule.destinations;
-    const destinationLength = destinations.length;
-    const lastDestinationLength = destinations[destinationLength - 1].length;
-    const firstDestination = destinations[0][0];
-    const lastDestination =
-      destinations[destinationLength - 1][lastDestinationLength - 1];
-    const destinationCount =
-      destinations.reduce((acc, cur) => acc + cur.length, 0) - 2;
-    setFirstDestination(firstDestination);
-    setLastDestination(lastDestination);
-    setDestinationCount(destinationCount < 0 ? 0 : destinationCount);
-  }, []);
-
   function handleDelete(e: React.MouseEvent<SVGElement>) {
     e.preventDefault();
     confirm('일정을 삭제하시겠습니까?');
+  }
+
+  function getDate(dateString: string) {
+    const toDate = new Date(dateString);
+    const year = toDate.getFullYear();
+    const month = toDate.getMonth();
+    const day = toDate.getDate();
+    return `${year}년 ${month}월 ${day}일`;
   }
 
   return (
@@ -38,9 +28,10 @@ function ScheduleCard({ schedule, link }: MyScheduleCardProps) {
             <div>
               {schedule.duration - 1}박 {schedule.duration}일
             </div>
-            <div>{schedule.start_date}</div>
-            <div>{schedule.end_date}</div>
-            <div>등록 : {schedule.created_at}</div>
+            <div>
+              {getDate(schedule.start_date)} ~ {getDate(schedule.end_date)}
+            </div>
+            <div>등록 : {getDate(schedule.created_at)}</div>
           </div>
           <div className={styles.like}>
             <FaTrashAlt
@@ -52,13 +43,34 @@ function ScheduleCard({ schedule, link }: MyScheduleCardProps) {
           </div>
         </div>
         <div className={styles.scheduleDestination}>
-          <div>{firstDestination}</div>
+          <div>
+            {schedule.first_destination
+              ? schedule.first_destination
+              : '출발지가 필요합니다'}
+          </div>
           <div>{'=>'}</div>
-          <div className={styles.destinationCount}>경유 {destinationCount}</div>
+          <div className={styles.destinationCount}>
+            경유{' '}
+            {schedule.destination_count - 2 > 0
+              ? schedule.destination_count - 2
+              : 0}
+          </div>
           <div>{'=>'}</div>
-          <div>{lastDestination}</div>
+          <div>
+            {schedule.last_destination
+              ? schedule.last_destination
+              : '도착지가 필요합니다'}
+          </div>
         </div>
-        <img src={schedule.image} className={styles.image} />
+        <img
+          src={
+            schedule.image
+              ? schedule.image
+              : 'https://www.agoda.com/wp-content/uploads/2020/04/Jeju-Island-hotels-things-to-do-in-Jeju-Island-South-Korea.jpg'
+          }
+          className={styles.image}
+          alt='일정 배경 사진'
+        />
       </div>
     </Link>
   );
