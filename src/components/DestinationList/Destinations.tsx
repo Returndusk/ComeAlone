08a3fa33cc396f8094a986ab897c35360b36dab6
Jeongeun Lee <leeju1009@gmail.com>
2023-06-08 14:25:51 +0900

    :bug: 최초렌더링 시, 목적지리스트가 페이지네이션 되지 않는 문제 수정
    최초로 목 페이지에 접속했을 때, 목적지 리스트가 페이지네이션 되지 않는 문제 수정

--- src/components/DestinationList/Destinations.tsx
+++ src/components/DestinationList/Destinations.tsx
@@ -9,16 +9,16 @@ import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 
 type DestinationsPropsType = {
   filteredDestinations: DestinationsType[] | [];
-  isLoading: boolean;
-  setIsLoading: React.Dispatch<React.SetStateAction<boolean>>;
+  // isLoading: boolean;
+  // setIsLoading: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
 //해야할거: 랭킹 데이터, 서치데이터, 카테고리 데이터 랭킹 규정하기
 function Destinations({
-  filteredDestinations,
-  isLoading,
-  setIsLoading
-}: DestinationsPropsType) {
+  filteredDestinations
+}: // isLoading,
+// setIsLoading
+DestinationsPropsType) {
   const [slicedDestinations, setSlicedDestinations] = useState<
     DestinationsType[] | []
   >(filteredDestinations);
@@ -35,10 +35,6 @@ function Destinations({
   useEffect(() => {
     setDetailsDomRoot(() => document.getElementById('main'));
   }, []);
-
-  useEffect(() => {
-    setSlicedDestinations(() => filteredDestinations);
-  }, [filteredDestinations]);
 
   const handleDestinationClick = (destination: DestinationsType) => {
     setClickedDestination(() => destination);
@@ -58,71 +54,71 @@ function Destinations({
 
   return (
     <div className={styles.destinationContentsContainer}>
-      {isLoading ? (
+      {/* {isLoading ? (
         <div>로딩 중..</div>
-      ) : (
-        <div className={styles.destinationsInfoContainer}>
-          <section className={styles.destinationsContainer}>
-            {filteredDestinations.length > 0 ? (
-              <>
-                {slicedDestinations.map(
-                  (destination: DestinationsType, index) => (
-                    <div
-                      key={index}
-                      className={styles.destinations}
-                      onClick={() => handleDestinationClick(destination)}
-                    >
-                      <h3>{destination?.title}</h3>
-                      <p>{destination?.addr1}</p>
-                      <p>{destination?.category_id}</p>
-
-                      {imageError ? (
-                        <img
-                          id={styles.destinationImage}
-                          src={destination?.image1}
-                          alt={destination.title}
-                        />
-                      ) : (
-                        <img
-                          id={styles.destinationImage}
-                          src={destination?.image2}
-                          alt={destination.title}
-                          onError={handleImageError}
-                        />
-                      )}
-                    </div>
-                  )
-                )}
-                <Pagination
-                  filteredDestinations={filteredDestinations}
-                  setSlicedDestinations={setSlicedDestinations}
-                />
-              </>
-            ) : (
-              <div className={styles.alertContainer}>
-                <CiCircleAlert className={styles.alertIcon} />
-                <p>검색 결과가 없습니다.</p>
-              </div>
-            )}
-          </section>
-          {isOpen &&
-            detailsDomRoot !== null &&
-            createPortal(
-              <section className={styles.detailsContainer}>
-                <Outlet />
-                <div className={styles.detailsButtonContainer}>
-                  <button
-                    className={styles.detailsCloseButton}
-                    onClick={closeDetailPage}
+      ) : ( */}
+      <div className={styles.destinationsInfoContainer}>
+        <section className={styles.destinationsContainer}>
+          {filteredDestinations.length > 0 ? (
+            <>
+              {slicedDestinations.map(
+                (destination: DestinationsType, index) => (
+                  <div
+                    key={index}
+                    className={styles.destinations}
+                    onClick={() => handleDestinationClick(destination)}
                   >
-                    X
-                  </button>
-                </div>
-              </section>,
-              detailsDomRoot
-            )}
-        </div>
-      )}
+                    <h3>{destination?.title}</h3>
+                    <p>{destination?.addr1}</p>
+                    <p>{destination?.category_id}</p>
+
+                    {imageError ? (
+                      <img
+                        id={styles.destinationImage}
+                        src={destination?.image1}
+                        alt={destination.title}
+                      />
+                    ) : (
+                      <img
+                        id={styles.destinationImage}
+                        src={destination?.image2}
+                        alt={destination.title}
+                        onError={handleImageError}
+                      />
+                    )}
+                  </div>
+                )
+              )}
+              <Pagination
+                filteredDestinations={filteredDestinations}
+                setSlicedDestinations={setSlicedDestinations}
+              />
+            </>
+          ) : (
+            <div className={styles.alertContainer}>
+              <CiCircleAlert className={styles.alertIcon} />
+              <p>검색 결과가 없습니다.</p>
+            </div>
+          )}
+        </section>
+        {isOpen &&
+          detailsDomRoot !== null &&
+          createPortal(
+            <section className={styles.detailsContainer}>
+              <Outlet />
+              <div className={styles.detailsButtonContainer}>
+                <button
+                  className={styles.detailsCloseButton}
+                  onClick={closeDetailPage}
+                >
+                  X
+                </button>
+              </div>
+            </section>,
+            detailsDomRoot
+          )}
+      </div>
+      {/* )} */}
       <Map
         markersLocations={
           clickedDestination !== null
