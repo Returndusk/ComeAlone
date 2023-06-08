5ba9d00989f5ae5beb47055555a13b35eefdfc27
Jeongeun Lee <leeju1009@gmail.com>
2023-06-08 22:08:43 +0900

    :label: 목적지 상세정보 데이터 타입 추가
    DestinationDetails 타입 새로 추가

--- src/types/DestinationListTypes.ts
+++ src/types/DestinationListTypes.ts
@@ -1,18 +1,23 @@
 //dummy data 제거 후 옵션 -> 필수로 변경 예정
 export interface DestinationsType {
-  id?: number; // 구 contentid
-  category_id?: number; // 구 contenttypeid
+  id: number; // 구 contentid
+  category_id: number; // 구 contenttypeid
   title: string;
-  homepage?: string;
-  tel?: string;
-  image1?: string;
-  image2?: string;
-  addr1?: string;
-  addr2?: string;
-  zipcode?: string;
+  homepage: string;
+  tel: string;
+  image1: string;
+  image2: string;
+  addr1: string;
+  addr2: string;
+  zipcode: string;
   mapx: number; //dummy data 제거 후 string으로 수정 예정 (API에서는 string)
   mapy: number; //dummy data 제거 후 string으로 수정 예정 (API에서는 string)
-  overview?: string;
+  overview: string;
+}
+
+export interface DestinationsDetailsType extends DestinationsType {
+  destination_comments: string[] | [];
+  comment_count: number;
 }
 
 export interface DestinationsReviewType {
