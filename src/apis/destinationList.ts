dec95e6428e57f871bd579da60e2fad35517cf76
Jeongeun Lee <leeju1009@gmail.com>
2023-06-08 20:19:15 +0900

    :construction: API 갱신
    좋아요 요청, 리뷰 정보를 받아오는 API 추가

--- src/apis/destinationList.ts
+++ src/apis/destinationList.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import tokenInstance from './tokenInstance';
 
 const baseUrl = process.env.REACT_APP_API_BASE_URL;
 
@@ -14,25 +15,59 @@ export const getAllDestinationsList = async () => {
 export const getDestinationsListByCategoryId = async (
   selectedCategory: number[]
 ) => {
-  const categryIdLists = selectedCategory.join();
-  const response = await axios.get(
-    `${baseUrl}/categories/destinations?categoryIds=${categryIdLists}`
-  );
-  return response;
+  try {
+    const categryIdLists = selectedCategory.join();
+    const response = await axios.get(
+      `${baseUrl}/categories/destinations?categoryIds=${categryIdLists}`
+    );
+    return response;
+  } catch (error) {
+    console.error(
+      error,
+      '카테고리별 목적지 리스트 정보를 받아오는데 실패했습니다.'
+    );
+  }
 };
 
 export const getDestinationDetailsByDestinationId = async (
   destinationId: number
 ) => {
-  const response = await axios.get(`${baseUrl}/destinations/${destinationId}`);
-  return response;
+  try {
+    const response = await axios.get(
+      `${baseUrl}/destinations-with-reviews/${destinationId}`
+    );
+    return response;
+  } catch (error) {
+    console.error(error, '목적지별 상세정보를 받아오는데 실패했습니다.');
+  }
 };
 
 export const getRankedDestinationsByRankingNumber = async (
   rankingNumber: number
 ) => {
-  const response = await axios.get(
-    `${baseUrl}/ranking/destinations?count=${rankingNumber}`
-  );
-  return response;
+  try {
+    const response = await axios.get(
+      `${baseUrl}/ranking/destinations?count=${rankingNumber}`
+    );
+    return response;
+  } catch (error) {
+    console.error(
+      error,
+      ' 좋아요가 높은 목적지 리스트를 받아오는데 실패했습니다.'
+    );
+  }
+};
+
+export const postPreferredDestinationsByDestinationId = async (
+  destinationId: number
+) => {
+  try {
+    const response = await tokenInstance.post(
+      `${baseUrl}/destinations/${destinationId}/likes`,
+      destinationId
+    );
+    return response;
+  } catch (error) {
+    console.error(error, ' 목적지 좋아요 설정/해제를 실패했습니다.');
+  }
 };
