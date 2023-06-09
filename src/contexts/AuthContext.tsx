e97a7000076128fbd6e780eae5e044e1203158c1
icallitnewart <icallitnewart@gmail.com>
2023-06-09 19:14:07 +0900

    :bug: 페이지 이동시 로그인 상태 업데이트 조건 변경
    로그인 상태이나 리프레시 토큰은 없는 경우 사용자 정보 갱신되게끔 수정

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -99,11 +99,11 @@ export function AuthProvider({ children }: AuthProviderProps) {
     }
   }, []);
 
-  //url 변경시 리프레시 토큰이 없을 경우 상태 업데이트
+  //url 변경시 isLoggedIn이 true인데 리프레시 토큰이 없을 경우 상태 업데이트
   useEffect(() => {
     const refreshToken = cookies.get('refreshToken');
 
-    if (!refreshToken) {
+    if (!refreshToken && authState.isLoggedIn) {
       updateAuthState(false);
     }
   }, [location]);
