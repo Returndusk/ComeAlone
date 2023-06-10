c5bcda7d9bb2aa40596506e053c4a7a60fda23de
ll-ss <leesw0529@gmail.com>
2023-06-10 13:46:47 +0900

    💄 상세 페이지 리뷰 수정하기, 삭제하기 버튼 추가
    로그인된 유저와 리뷰 작성자가 같은 경우에만 렌더링됨

--- src/components/ScheduleDetail/ReviewsSchedule.tsx
+++ src/components/ScheduleDetail/ReviewsSchedule.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './ReviewsSchedule.module.scss';
+import { useAuthState } from '../../contexts/AuthContext';
 import Avatar from '@mui/material/Avatar';
 import { ScheduleReviewPropsType } from '../../types/ScheduleDetailTypes';
 
@@ -8,22 +9,30 @@ function ReviewsSchedule({
 }: {
   scheduleReviews: ScheduleReviewPropsType[];
 }) {
+  const loggedInUserId = useAuthState().authState.user?.id;
+
   return (
     <div className={styles.reviewsContainer}>
       <div className={styles.reviewsTitle}>리뷰 리스트</div>
       <div className={styles.reviewsList}>
         {scheduleReviews.map((review, index) => {
-          console.log(review);
-
           return (
             <div key={`review ${index}`} className={styles.review}>
               <span className={styles.nickname}>
                 <Avatar>{review.user.nickname[0]}</Avatar>
               </span>
               <span>{review.comment}</span>
-              <span className={styles.createdAt}>
-                {review.created_at.split('T')[0]}
-              </span>
+              <div className={styles.buttonsContainer}>
+                {loggedInUserId === review.user.id ? (
+                  <>
+                    <button className={styles.updateButton}>수정하기</button>
+                    <button className={styles.deleteButton}>삭제하기</button>
+                  </>
+                ) : null}
+                <span className={styles.createdAt}>
+                  {review.created_at.split('T')[0]}
+                </span>
+              </div>
             </div>
           );
         })}
