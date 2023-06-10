cdf6acf3f4f72e50ffc067c8163df63ee4ef381c
ll-ss <leesw0529@gmail.com>
2023-06-10 11:27:37 +0900

    💄 상세 페이지 비로그인 시 좋아요 버튼에 띄우는 메시지 추가

--- src/components/ScheduleDetail/IconsScheduleDetail.tsx
+++ src/components/ScheduleDetail/IconsScheduleDetail.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './IconsScheduleDetail.module.scss';
 import { useAuthState } from '../../contexts/AuthContext';
+import { Tooltip } from '@mui/material';
 import { FaRegHeart, FaHeart, FaCommentAlt } from 'react-icons/fa';
 import { IconsScheduleDetailType } from '../../types/ScheduleDetailTypes';
 
@@ -14,23 +15,33 @@ function IconsScheduleDetail({
 
   return (
     <div className={styles.iconsContainer}>
-      <button
-        id={authState.isLoggedIn ? styles.likes : styles.likesDisabled}
-        onClick={() => {
-          if (authState.isLoggedIn) {
+      {authState.isLoggedIn ? (
+        <button
+          id={styles.likes}
+          onClick={() => {
             onUserLike();
-          } else {
-            return;
-          }
-        }}
-      >
-        {doesUserLike ? (
-          <FaHeart id={styles.likesIcon} />
-        ) : (
-          <FaRegHeart id={styles.likesIcon} />
-        )}
-        {likesCount}
-      </button>
+          }}
+        >
+          {doesUserLike ? (
+            <FaHeart id={styles.likesIcon} />
+          ) : (
+            <FaRegHeart id={styles.likesIcon} />
+          )}
+          {likesCount}
+        </button>
+      ) : (
+        <Tooltip title='좋아요 하시려면 로그인해주세요.' placement='top'>
+          <button
+            id={styles.likesDisabled}
+            onClick={() => {
+              return;
+            }}
+          >
+            <FaRegHeart id={styles.likesIcon} />
+            {likesCount}
+          </button>
+        </Tooltip>
+      )}
       <span id={styles.reviewNumber}>
         <FaCommentAlt id={styles.reviewNumberIcon} />
         {reviewsAmount}
