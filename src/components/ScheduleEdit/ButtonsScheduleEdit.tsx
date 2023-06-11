94f7ac5b65c5c3e003d3d76eb230b29c70bab4cd
ll-ss <leesw0529@gmail.com>
2023-06-11 15:39:57 +0900

    ✨ 일정 삭제 시 확인 모달창 구현

--- src/components/ScheduleEdit/ButtonsScheduleEdit.tsx
+++ src/components/ScheduleEdit/ButtonsScheduleEdit.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './ButtonsScheduleEdit.module.scss';
+import AlertModal from '../common/Alert/AlertModal';
 
 function ButtonsScheduleEdit({
   onSubmit,
@@ -8,14 +9,32 @@ function ButtonsScheduleEdit({
   onSubmit: () => void;
   onDelete: () => void;
 }) {
+  const [showDeleteAlert, setShowDeleteAlert] = useState(false);
+
+  const handleDelete = () => {
+    onDelete();
+    setShowDeleteAlert(false);
+  };
+
   return (
     <div className={styles.buttonsContainer}>
-      <button className={styles.deleteButton} onClick={() => onDelete()}>
+      <button
+        className={styles.deleteButton}
+        onClick={() => setShowDeleteAlert(true)}
+      >
         삭제하기
       </button>
       <button className={styles.confirmButton} onClick={() => onSubmit()}>
         수정완료
       </button>
+      {showDeleteAlert && (
+        <AlertModal
+          message='해당 일정을 삭제하시겠습니까?'
+          showCancelButton={true}
+          onConfirm={handleDelete}
+          onCancel={() => setShowDeleteAlert(false)}
+        />
+      )}
     </div>
   );
 }
