fb9b3643d7a61ad506e651e49cff46b6acbea697
ll-ss <leesw0529@gmail.com>
2023-06-11 16:23:41 +0900

    💄 상세 페이지에 일정 삭제 버튼 추가 및 style 변경

--- src/components/ScheduleDetail/ButtonsScheduleDetail.tsx
+++ src/components/ScheduleDetail/ButtonsScheduleDetail.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './ButtonsScheduleDetail.module.scss';
 import ROUTER from '../../constants/Router';
+import { FaPen, FaTrashAlt } from 'react-icons/fa';
 
 function ButtonsScheduleDetail({
   userId,
@@ -13,14 +14,20 @@ function ButtonsScheduleDetail({
   scheduleId: string | undefined;
 }) {
   return (
-    <div className={styles.editButtonContainer}>
+    <div className={styles.buttonContainer}>
       {userId === loggedInUserId ? (
-        <Link
-          to={`${ROUTER.SCHEDULE_EDIT}/${scheduleId}`}
-          className={styles.editButton}
-        >
-          수정하기
-        </Link>
+        <>
+          <button id={styles.editButton}>
+            <Link to={`${ROUTER.SCHEDULE_EDIT}/${scheduleId}`}>
+              <FaPen className={styles.icon} />
+              수정
+            </Link>
+          </button>
+          <button id={styles.deleteButton}>
+            <FaTrashAlt className={styles.icon} />
+            삭제
+          </button>
+        </>
       ) : null}
     </div>
   );
