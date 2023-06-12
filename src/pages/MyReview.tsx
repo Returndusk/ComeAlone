86a2682323824ec8214c955621c44a5c86accc1f
Jeongeun Lee <leeju1009@gmail.com>
2023-06-12 20:49:43 +0900

    :truck: 로그인한 유저가 자신의 리뷰를 확인하는 기능을 사용자 페이지로 이동
    로그인한 유저가 자신의 리뷰를 확인할 수 있는 기능을 사용자 페이지로 이동시킴

--- src/pages/MyReview.tsx
+++ src/pages/MyReview.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from '../components/MyReview/MyReview.module.scss';
+import UsersReview from '../components/DestinationList/UsersReview';
 
 function MyReview() {
   return (
     <main className={styles.container}>
       <div className={styles.wrapper}>
         <h1>마이 리뷰</h1>
+        <UsersReview />
       </div>
     </main>
   );
