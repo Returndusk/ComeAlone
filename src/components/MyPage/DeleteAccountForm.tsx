75ef7ddc9f1c92d401a57bc18bddb6060fa615ea
icallitnewart <icallitnewart@gmail.com>
2023-06-13 19:23:18 +0900

    :sparkles: 마이페이지 alert창 AlertModal 컴포넌트로 대체

--- src/components/MyPage/DeleteAccountForm.tsx
+++ src/components/MyPage/DeleteAccountForm.tsx
@@ -4,6 +4,13 @@ import { TextField } from '@mui/material';
 import { AxiosError } from 'axios';
 import { checkPassword, deleteUser } from '../../apis/UserAPI';
 import { useAuthState } from '../../contexts/AuthContext';
+import AlertModal from '../common/Alert/AlertModal';
+
+type AlertOption = {
+  isOpen: boolean;
+  message: string;
+  onConfirm: null | (() => void);
+};
 
 type DeleteFormProps = {
   cancelDeleteAccount: () => void;
@@ -13,6 +20,12 @@ function DeleteAccountForm({ cancelDeleteAccount }: DeleteFormProps) {
   const { updateAuthState } = useAuthState();
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
+  const initAlert = {
+    isOpen: false,
+    message: '',
+    onConfirm: null
+  };
+  const [alertModal, setAlertModal] = useState<AlertOption>(initAlert);
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setPassword(e.target.value);
   };
@@ -28,12 +41,22 @@ function DeleteAccountForm({ cancelDeleteAccount }: DeleteFormProps) {
       try {
         const response = await deleteUser();
         if (response.status === 200) {
-          alert('탈퇴 완료되었습니다.');
-          updateAuthState(false);
+          setAlertModal({
+            isOpen: true,
+            message: '탈퇴 완료되었습니다.',
+            onConfirm: () => {
+              updateAuthState(false);
+              // navigate('/');
+            }
+          });
         }
       } catch (err) {
         console.log(err);
-        alert('회원 탈퇴에 실패하였습니다.');
+        setAlertModal({
+          isOpen: true,
+          message: '회원 탈퇴에 실패하였습니다.',
+          onConfirm: () => setAlertModal(initAlert)
+        });
       }
     }
   };
@@ -58,60 +81,75 @@ function DeleteAccountForm({ cancelDeleteAccount }: DeleteFormProps) {
               err.response.data.reason === 'INVALID' ||
               err.response.data.reason === 'EXPIRED'
             ) {
-              alert('로그인 상태가 아닙니다. 다시 로그인해주세요.');
-              return updateAuthState(false);
+              return setAlertModal({
+                isOpen: true,
+                message: '로그인 상태가 아닙니다. 다시 로그인해주세요.',
+                onConfirm: () => updateAuthState(false)
+              });
             }
             return setError('비밀번호가 일치하지 않습니다.');
           }
         }
 
         console.log(err);
-        alert('비밀번호 확인에 실패하였습니다.');
+        setAlertModal({
+          isOpen: true,
+          message: '비밀번호 확인에 실패하였습니다.',
+          onConfirm: () => setAlertModal(initAlert)
+        });
       }
     }
   };
 
   return (
-    <form className={styles.form} onSubmit={handleSubmit}>
-      <div className={styles.input}>
-        <TextField
-          id='outlined-basic'
-          label='비밀번호 확인'
-          type='password'
-          variant='outlined'
-          name='password'
-          value={password}
-          onChange={handleChange}
-          size='small'
-          style={{ width: '100%' }}
-          sx={{
-            '& label.Mui-focused': { color: '#ef6d00' },
-            '& .MuiOutlinedInput-root': {
-              '&.Mui-focused fieldset': {
-                borderColor: '#fe9036',
-                borderWidth: '1px'
+    <>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <div className={styles.input}>
+          <TextField
+            id='outlined-basic'
+            label='비밀번호 확인'
+            type='password'
+            variant='outlined'
+            name='password'
+            value={password}
+            onChange={handleChange}
+            size='small'
+            style={{ width: '100%' }}
+            sx={{
+              '& label.Mui-focused': { color: '#ef6d00' },
+              '& .MuiOutlinedInput-root': {
+                '&.Mui-focused fieldset': {
+                  borderColor: '#fe9036',
+                  borderWidth: '1px'
+                }
               }
-            }
-          }}
+            }}
+          />
+          {!error && (
+            <p className={styles.msg}>
+              본인 확인을 위해 비밀번호를 입력해주세요.
+            </p>
+          )}
+          {error && <p className={styles.errMsg}>{error}</p>}
+        </div>
+        <ul className={styles.buttons}>
+          <li>
+            <button type='submit'>탈퇴하기</button>
+          </li>
+          <li>
+            <button className={styles.cancelBtn} onClick={cancelDeleteAccount}>
+              취소
+            </button>
+          </li>
+        </ul>
+      </form>
+      {alertModal.isOpen && alertModal.onConfirm && (
+        <AlertModal
+          message={alertModal.message}
+          onConfirm={alertModal.onConfirm}
         />
-        {!error && (
-          <p className={styles.msg}>
-            본인 확인을 위해 비밀번호를 입력해주세요.
-          </p>
-        )}
-        {error && <p className={styles.errMsg}>{error}</p>}
-      </div>
-      <ul className={styles.buttons}>
-        <li>
-          <button type='submit'>탈퇴하기</button>
-        </li>
-        <li>
-          <button className={styles.cancelBtn} onClick={cancelDeleteAccount}>
-            취소
-          </button>
-        </li>
-      </ul>
-    </form>
+      )}
+    </>
   );
 }
 
