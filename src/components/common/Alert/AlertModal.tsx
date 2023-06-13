eb0579bafdcbed324a3fb7e2b5131c3322583842
김득열 <emrfufrns@gmail.com>
2023-06-13 17:36:47 +0900

    :sparkles: 메인 페이지 기능 및 디자인 일부 수정
    정렬, 불필요한 요소 삭제 등등

--- src/components/common/Alert/AlertModal.tsx
+++ src/components/common/Alert/AlertModal.tsx
@@ -39,6 +39,7 @@ function AlertModal({
     <Dialog
       open
       TransitionComponent={Transition}
+      disableScrollLock
       PaperProps={{
         style: {
           position: 'absolute',
@@ -51,15 +52,57 @@ function AlertModal({
         }
       }}
     >
-      {showTitle && title && <DialogTitle>{title}</DialogTitle>}
+      {showTitle && title && (
+        <DialogTitle
+          style={{
+            fontSize: '20px',
+            textAlign: 'center',
+            color: '#ef6d00'
+          }}
+        >
+          {title}
+        </DialogTitle>
+      )}
       <DialogContent>
-        <DialogContentText>{message}</DialogContentText>
+        <DialogContentText
+          style={{
+            color: '#555',
+            fontSize: '15px',
+            fontWeight: '500'
+          }}
+        >
+          {message}
+        </DialogContentText>
       </DialogContent>
       <DialogActions>
         {showCancelButton && onCancel && (
-          <Button onClick={onCancel}>취소</Button>
+          <Button
+            sx={{
+              color: 'black',
+              fontWeight: '600',
+              '&:hover': {
+                color: 'white',
+                backgroundColor: 'black'
+              }
+            }}
+            onClick={onCancel}
+          >
+            취소
+          </Button>
         )}
-        <Button onClick={onConfirm}>확인</Button>
+        <Button
+          sx={{
+            color: '#ef6d00',
+            fontWeight: '600',
+            '&:hover': {
+              color: '#ffffff',
+              backgroundColor: '#ef6d00'
+            }
+          }}
+          onClick={onConfirm}
+        >
+          확인
+        </Button>
       </DialogActions>
     </Dialog>
   );
