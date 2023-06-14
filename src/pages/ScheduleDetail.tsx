535b4d41319b03ffa85269e7f26b57e43f15713d
ll-ss <leesw0529@gmail.com>
2023-06-14 12:27:30 +0900

    🐛 잘못된 id로 일정 상세 페이지 접근 시 예외 처리 추가

--- src/pages/ScheduleDetail.tsx
+++ src/pages/ScheduleDetail.tsx
@@ -1,12 +1,13 @@
 import React, { useState, useEffect, useCallback, useRef } from 'react';
 import { useAuthState } from '../contexts/AuthContext';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import styles from '../components/ScheduleDetail/ScheduleDetail.module.scss';
 import ImageScheduleDetail from '../components/ScheduleDetail/ImageScheduleDetail';
 import InfoScheduleDetail from '../components/ScheduleDetail/InfoScheduleDetail';
 import ButtonsScheduleDetail from '../components/ScheduleDetail/ButtonsScheduleDetail';
 import IconsScheduleDetail from '../components/ScheduleDetail/IconsScheduleDetail';
 import DestinationList from '../components/ScheduleDetail/DestinationList';
+import DestinationsMap from '../components/ScheduleDetail/DestinationsMap';
 import ReviewsSchedule from '../components/ScheduleDetail/ReviewsSchedule';
 import InputReviewSchedule from '../components/ScheduleDetail/InputReviewSchedule';
 import {
@@ -23,9 +24,11 @@ import {
   ScheduleFetchedType
 } from '../types/ScheduleDetailTypes';
 import { MapWithWaypointsPropsType } from '../types/DestinationListTypes';
-import DestinationsMap from '../components/ScheduleDetail/DestinationsMap';
+import ROUTER from '../constants/Router';
+import { AxiosError } from 'axios';
 
 function ScheduleDetail() {
+  const navigate = useNavigate();
   const scheduleId: string = useParams().scheduleId as string;
   const isLoggedIn: boolean = useAuthState().authState.isLoggedIn as boolean;
   const loggedInUserId: string = useAuthState().authState.user?.id as string;
@@ -39,27 +42,39 @@ function ScheduleDetail() {
   const userLikesCount = useRef<number>(0);
 
   const getScheduleDetail = useCallback(async (scheduleId: string) => {
-    const response = await getScheduleDetailById(scheduleId);
+    try {
+      const response = await getScheduleDetailById(scheduleId);
 
-    const data = {
-      userId: response?.data.user.id,
-      nickname: response?.data.user.nickname,
-      profileImage: response?.data.user.profile_image,
-      title: response?.data.title,
-      summary: response?.data.summary,
-      likesCount: response?.data.likes_count,
-      duration: response?.data.duration,
-      startDate: new Date(response?.data.start_date),
-      endDate: new Date(response?.data.end_date),
-      image: response?.data.image,
-      createdAt: new Date(response?.data.created_at.split('T')[0]),
-      updatedAt: new Date(response?.data.updated_at.split('T')[0]),
-      commentsCount: response?.data.comments_count,
-      destinationCount: response?.data.destination_count,
-      destinations: response?.data.destinationMaps
-    };
+      const data = {
+        userId: response?.data.user.id,
+        nickname: response?.data.user.nickname,
+        profileImage: response?.data.user.profile_image,
+        title: response?.data.title,
+        summary: response?.data.summary,
+        likesCount: response?.data.likes_count,
+        duration: response?.data.duration,
+        startDate: new Date(response?.data.start_date),
+        endDate: new Date(response?.data.end_date),
+        image: response?.data.image,
+        createdAt: new Date(response?.data.created_at.split('T')[0]),
+        updatedAt: new Date(response?.data.updated_at.split('T')[0]),
+        commentsCount: response?.data.comments_count,
+        destinationCount: response?.data.destination_count,
+        destinations: response?.data.destinationMaps
+      };
 
-    return data;
+      return data;
+    } catch (err: unknown) {
+      if (err instanceof AxiosError) {
+        if (err.response?.status === 404) {
+          console.log(err.response.data.message);
+
+          navigate(ROUTER.MAIN);
+        }
+      } else {
+        console.log(err);
+      }
+    }
   }, []);
 
   const getDoesUserLike = useCallback(async (scheduleId: string) => {
@@ -107,14 +122,19 @@ function ScheduleDetail() {
 
   useEffect(() => {
     const fetchData = async () => {
-      const fetchedDetail: ScheduleFetchedType = await getScheduleDetail(
+      const fetchedDetail = (await getScheduleDetail(
         scheduleId
-      );
+      )) as ScheduleFetchedType;
+
+      if (!fetchedDetail) {
+        return;
+      }
+
       const fetchedReviews: IScheduleReview[] = await getScheduleReviews(
         scheduleId
       );
 
-      scheduleFetched.current = fetchedDetail;
+      scheduleFetched.current = fetchedDetail as ScheduleFetchedType;
       userLikesCount.current = fetchedDetail.likesCount;
       setScheduleReviews(fetchedReviews);
       setCheckedDestinations(fetchedDetail.destinations.flat());
@@ -128,7 +148,7 @@ function ScheduleDetail() {
     };
 
     fetchData();
-  }, []);
+  }, [getScheduleDetail, getScheduleReviews, getDoesUserLike, isLoggedIn]);
 
   const handleReviewSubmit = (input: string) => {
     addScheduleReview(scheduleId, input);
