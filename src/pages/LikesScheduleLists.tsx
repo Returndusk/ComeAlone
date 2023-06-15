8975aeb0ac199d0bcfdbee83e6edd61aefd9167d
[SW4/2반] 이지원 <jiwonlee1953@naver.com>
2023-06-15 11:36:16 +0900

    :sparkles: 여행 일정 페이지 무한스크롤 구현
    여행 일정 페이지 무한스크롤 구현

---
+++ src/pages/LikesScheduleLists.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import LikesScheduleLists from '../components/ScheduleList/LikesScheduleList';
+import ScrollTop from '../components/common/ScrollTop/ScrollTop';
+
+function LikesScheduleList() {
+  return (
+    <div>
+      <LikesScheduleLists />
+      <ScrollTop />
+    </div>
+  );
+}
+
+export default LikesScheduleList;
