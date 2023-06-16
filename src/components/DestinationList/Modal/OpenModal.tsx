2279ab1e468eb5a63128eabf2ab762ab77e98b40
JungHyun <wjstkwhwjdgus@gmail.com>
2023-06-16 11:00:55 +0900

    :pencil2: 주석처리

--- src/components/DestinationList/Modal/OpenModal.tsx
+++ src/components/DestinationList/Modal/OpenModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import ModalScheduleLists from './ModalScheduleLists';
 import styles from './OpenModal.module.scss';
 import { TfiClose } from 'react-icons/tfi';
